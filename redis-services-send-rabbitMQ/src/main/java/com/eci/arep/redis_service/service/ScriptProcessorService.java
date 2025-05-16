a6da1fb98e32bafc88398588428946c63b33e2f8
Jefferson Mesa <mesajefer2@gmail.com>
2025-05-15 20:20:41 -0500

    add scirpt for consume in concurrent this aplicaction

--- redis-services-send-rabbitMQ/src/main/java/com/eci/arep/redis_service/service/ScriptProcessorService.java
+++ redis-services-send-rabbitMQ/src/main/java/com/eci/arep/redis_service/service/ScriptProcessorService.java
@@ -15,6 +15,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class ScriptProcessorService {
+    private static int count = 0;
+    private static int countPremium = 0;
+    private static int countError = 0;
     private final RedisService redisService;
     //private final RabbitPublisherService rabbitPublisher;
     private static final Logger log = LoggerFactory.getLogger(ScriptProcessorService.class);
@@ -57,13 +60,19 @@ public class ScriptProcessorService {
             log.info("Enviando mensaje JSON: '{}' a exchange '{}' con routing key '{}'", json, exchangeName, routingKey);
             if (payload.getIsPremium()){
                 log.info("Enviando mensaje a cola premium");
+                countPremium++;
+                System.out.println(">>> Mensaje Procesado por la COLA PREMIUM: " + countPremium +" y el mensaje es:" + json);
                 rabbitTemplate.convertAndSend(exchangeName, routingKeyQueuePremium, json);
             } else {
                 log.info("Enviando mensaje a cola normal");
+                count++;
+                System.out.println(">>> Mensaje Procesado a normal van: "+ count +" y el mensaje es:" + json);
                 rabbitTemplate.convertAndSend(exchangeName, routingKey, json);
                 
             }
         } catch (JsonProcessingException e) {
+            countError++;
+            System.out.println(">>> Mensaje Procesado a ERROR van: "+ countError +" y el mensaje es:" + payload);
             log.error("Error serializando payload a JSON", e);
         }
         return "processed of script of 5000 ml: " + payload.getData();
--- restaurant-service-receive-rabbitMQ/src/main/java/com/eci/arep/restaurant_service/listener/MessageListener.java
+++ restaurant-service-receive-rabbitMQ/src/main/java/com/eci/arep/restaurant_service/listener/MessageListener.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MessageListener {
+    private int count = 0;
+    private int countPremium = 0;
 
     private static final Logger log = LoggerFactory.getLogger(MessageListener.class);
 
@@ -16,15 +18,17 @@ public class MessageListener {
         log.info("Mensaje recibido a normal: '{}'", message);
         // Aquí puedes añadir la lógica para procesar el mensaje
         // Por ejemplo: guardar en base de datos, llamar a otra API, etc.
-        System.out.println(">>> Mensaje Procesado a normal: " + message);
+        count++;
+        System.out.println(">>> Mensaje Procesado a normal van: "+ count +" y el mensaje es:" + message);
     }
 
     // Escucha en la cola definida en application.properties (a través de la config)
-    @RabbitListener(queues = "${app.rabbitmq.queuePremium}")
+    @RabbitListener(queues = "${app.rabbitmq.queuePremium}") 
     public void receiveMessageQueuePremium(String message) {
         log.info("Mensaje recibido a la COLA PREMIUM: '{}'", message);
         // Aquí puedes añadir la lógica para procesar el mensaje
         // Por ejemplo: guardar en base de datos, llamar a otra API, etc.
-        System.out.println(">>> Mensaje Procesado por la COLA PREMIUM: " + message);
+        countPremium++;
+        System.out.println(">>> Mensaje Procesado por la COLA PREMIUM: " + countPremium +" y el mensaje es:" + message);
     }
 }
